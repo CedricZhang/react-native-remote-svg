873e598ef99a649d4f9d45e23a7637d8ff94d5b0
Cedric Zhang <zhebin.zhang@siuvo.com>
2018-09-20 16:01:56 -0400

    fix scale bugs on Android

--- SvgImage.js
+++ SvgImage.js
@@ -4,16 +4,30 @@ import React, { Component } from 'react';
 import { View, WebView } from 'react-native';
 
 const firstHtml =
-  '<html><head><style>html, body { margin:0; padding:0; overflow:hidden; background-color: transparent; } svg { position:fixed; top:0; left:0; height:100%; width:100% }</style></head><body>';
-const lastHtml = '</body></html>';
+  `<html>
+    <head>
+      <style>
+        html, body 
+        { margin:0; padding:0; overflow:hidden; background-color: transparent; } 
+        svg { position:relative !important; top:0; left:0; height:100%; width:100%; }
+      </style>
+    </head>
+    <body><div style="width:100%; background-color:transparent">`;
+const lastHtml = '</div></body></html>';
 
 class SvgImage extends Component {
-  state = { fetchingUrl: null, svgContent: null };
+  state = { fetchingUrl: null, svgContent: null, svgUrl:null };
   componentDidMount() {
-    this.doFetch(this.props);
+    // this.doFetch(this.props);
+    this.setState({
+      svgUrl:this.props.source && this.props.source.uri
+    })
   }
   componentWillReceiveProps(nextProps) {
     this.doFetch(nextProps);
+    this.setState({
+      svgUrl:nextProps.source && nextProps.source.uri
+    })
   }
   doFetch = props => {
     let uri = props.source && props.source.uri;
@@ -22,7 +36,7 @@ class SvgImage extends Component {
         const index = uri.indexOf('<svg');
         this.setState({ fetchingUrl: uri, svgContent: uri.slice(index) });
       } else {
-        console.log('fetching', uri);
+        // console.log('fetching', uri);
         fetch(uri)
           .then(res => res.text())
           .then(text => {
@@ -37,6 +51,11 @@ class SvgImage extends Component {
   render() {
     const props = this.props;
     const { svgContent } = this.state;
+
+    // let html = `${firstHtml}${svgContent}${lastHtml}`
+    let html = `${firstHtml}<img src="${this.state.svgUrl}" style="width:100%; ">${lastHtml}`
+
+    // console.log("HTML", html)
     if (svgContent) {
       return (
         <View pointerEvents="none" style={[props.style, props.containerStyle]}>
@@ -52,7 +71,7 @@ class SvgImage extends Component {
               props.style,
             ]}
             scrollEnabled={false}
-            source={{ html: `${firstHtml}${svgContent}${lastHtml}` }}
+            source={{ html:html  }}
           />
         </View>
       );
